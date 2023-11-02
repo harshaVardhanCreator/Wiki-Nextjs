92d69883a362a306971d0cfd44bac1407070ead1
HarshaVardhanK <harshavardhank6222@gmail.com>
2023-11-02 13:53:00 +0530

    Wiki Next App with Search Bar and its Results

---
+++ app/[searchTerm]/loading.tsx
@@ -0,0 +1,9 @@
+export default function loading() {
+    return (
+        <main className="bg-slate-200 mx-auto max-w-lg p-1 min-h-screen flex justify-center">
+            <h2 className="m-4 text-2xl font-bold">
+                Loading...
+            </h2>
+        </main>
+    )
+}
